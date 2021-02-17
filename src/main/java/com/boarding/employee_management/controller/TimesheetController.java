f9615cf4131d214068f57568c5d5722c58351fa1
gabrielbutnariu <butnariu.gabriel24@gmail.com>
2021-02-17 15:54:53 +0200

    added TimesheetService and some refactoring

--- src/main/java/com/boarding/employee_management/controller/TimesheetController.java
+++ src/main/java/com/boarding/employee_management/controller/TimesheetController.java
@@ -1,76 +1,64 @@
 package com.boarding.employee_management.controller;
 
-import com.boarding.employee_management.handler.TimesheetNotFoundException;
-import com.boarding.employee_management.models.Timesheet;
-import com.boarding.employee_management.repositories.TimesheetRepository;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+        import com.boarding.employee_management.handler.TimesheetNotFoundException;
+        import com.boarding.employee_management.models.Timesheet;
+        import com.boarding.employee_management.repositories.TimesheetRepository;
+        import com.boarding.employee_management.services.TimesheetService;
+        import org.springframework.beans.BeanUtils;
+        import org.springframework.beans.BeanWrapper;
+        import org.springframework.beans.BeanWrapperImpl;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+        import javax.transaction.Transactional;
+        import java.util.HashSet;
+        import java.util.List;
+        import java.util.Set;
 
 @RestController
 @RequestMapping("/timesheet")
 public class TimesheetController {
     @Autowired
-    private TimesheetRepository timesheetRepository;
+    private TimesheetService timesheetService;
 
     @GetMapping
-    public List<Timesheet> list(){
-        return timesheetRepository.findAll();
+    public List<Timesheet> listAll(){
+        return timesheetService.list();
     }
 
     @GetMapping
     @RequestMapping("{emp_id}")
-    public List<Timesheet> getByEmp_id(@PathVariable Long emp_id){
-        return timesheetRepository.findAllByEmployeeId(emp_id);
+    public List<Timesheet> listByEmployeeId(@PathVariable Long emp_id){
+        return timesheetService.getByEmp_id(emp_id);
     }
 
+    //this one can be used to add checkin and checkout or just checkin
     @PostMapping("{emp_id}/checkin")
     @ResponseStatus(HttpStatus.CREATED)
-    public Timesheet addCheckin_date(@RequestBody final Timesheet timesheet){
-        return timesheetRepository.saveAndFlush(timesheet);
+    public Timesheet addEntry(@RequestBody final Timesheet timesheet){
+        return timesheetService.addCheckinDate(timesheet);
     }
+
     @RequestMapping(value = "{emp_id}/checkout",method = RequestMethod.PUT)
-    public Timesheet addCheckout_date(@PathVariable Long emp_id,@RequestBody Timesheet timesheet){
-        Timesheet timesheetToUpdate = timesheetRepository.findByEmployeeIdAndCheckoutDateNull(emp_id);
-        copyNonNullProperties(timesheetToUpdate,timesheet);
-        return timesheetRepository.saveAndFlush(timesheetToUpdate);
+    @ResponseStatus(HttpStatus.OK)
+    public Timesheet updateCheckoutDate(@PathVariable Long emp_id,@RequestBody Timesheet timesheet){
+        return timesheetService.addCheckout_date(emp_id,timesheet);
     }
 
-    public static void copyNonNullProperties(Object target,Object src) {
-        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
-    }
-
-    public static String[] getNullPropertyNames (Object source) {
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
-
-        Set<String> emptyNames = new HashSet<String>();
-        for(java.beans.PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
-        }
-        String[] result = new String[emptyNames.size()];
-        return emptyNames.toArray(result);
-    }
     //deleting all the entry for a specific employee
     @Transactional
     @RequestMapping(value = "{emp_id}/all",method = RequestMethod.DELETE)
-    public void deleteTimesheetByEmpId(@PathVariable Long emp_id){
-        timesheetRepository.deleteByEmployeeId(emp_id);
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteByEmployeeId(@PathVariable Long emp_id){
+        timesheetService.deleteTimesheetByEmpId(emp_id);
     }
 
     //deleting one entry for a employee
     @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
-    public void deleteTimesheetById(@PathVariable Long id){
-        timesheetRepository.deleteById(id);
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteById(@PathVariable Long id){
+        timesheetService.deleteTimesheetById(id);
     }
 
 }
---
+++ src/main/java/com/boarding/employee_management/services/TimesheetService.java
@@ -0,0 +1,71 @@
+package com.boarding.employee_management.services;
+
+import com.boarding.employee_management.models.Timesheet;
+import com.boarding.employee_management.repositories.TimesheetRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class TimesheetService {
+
+    private TimesheetRepository timesheetRepository;
+
+    @Autowired
+    public TimesheetService(TimesheetRepository timesheetRepository) {
+        this.timesheetRepository = timesheetRepository;
+    }
+
+    public List<Timesheet> list() {
+        return timesheetRepository.findAll();
+    }
+
+    public List<Timesheet> getByEmp_id(@PathVariable Long emp_id){
+        return timesheetRepository.findAllByEmployeeId(emp_id);
+    }
+
+    public Timesheet addCheckinDate(@RequestBody final Timesheet timesheet){
+        return timesheetRepository.saveAndFlush(timesheet);
+    }
+
+    public Timesheet addCheckout_date(@PathVariable Long emp_id,@RequestBody Timesheet timesheet){
+        Timesheet timesheetToUpdate = timesheetRepository.findByEmployeeIdAndCheckoutDateNull(emp_id);
+        copyNonNullProperties(timesheetToUpdate,timesheet);
+        return timesheetRepository.saveAndFlush(timesheetToUpdate);
+    }
+
+    public static void copyNonNullProperties(Object target,Object src) {
+        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
+
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public void deleteTimesheetByEmpId(@PathVariable Long emp_id){
+        timesheetRepository.deleteByEmployeeId(emp_id);
+    }
+
+    public void deleteTimesheetById(@PathVariable Long id){
+        timesheetRepository.deleteById(id);
+    }
+
+
+}
