a3a303e449666fddf405e7cb1663735132a420d7
gabrielbutnariu <butnariu.gabriel24@gmail.com>
2021-02-18 11:14:17 +0200

    added lombok again

--- src/main/java/com/boarding/app/models/Employee.java
+++ src/main/java/com/boarding/app/models/Employee.java
@@ -1,11 +1,15 @@
 package com.boarding.app.models;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name="employees")
+@Getter @Setter
 public class Employee {
 
     @Id
@@ -15,38 +19,5 @@ public class Employee {
     private String lastName;
     private String ssn;
 
-    public Employee(){ }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
 
 }
--- src/main/java/com/boarding/app/models/Timesheet.java
+++ src/main/java/com/boarding/app/models/Timesheet.java
@@ -1,9 +1,13 @@
 package com.boarding.app.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Date;
 
 @Entity(name="timesheet")
+@Getter @Setter
 public class Timesheet {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,38 +18,6 @@ public class Timesheet {
     @JoinColumn(name="emp_id",referencedColumnName="id",nullable=false,unique=true)
     private Employee employee;
     public Timesheet() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getCheckinDate() {
-        return checkinDate;
-    }
-
-    public void setCheckinDate(Date checkinDate) {
-        this.checkinDate = checkinDate;
-    }
-
-    public Date getCheckoutDate() {
-        return checkoutDate;
-    }
-
-    public void setCheckoutDate(Date checkoutDate) {
-        this.checkoutDate = checkoutDate;
     }
 }
 
