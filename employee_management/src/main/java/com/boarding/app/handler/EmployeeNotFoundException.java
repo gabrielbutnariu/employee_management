fbe0de029d84a76b1b5bc25d9a213f9159e599c3
gabrielbutnariu <38704391+gabrielbutnariu@users.noreply.github.com>
2021-02-23 12:09:54 +0200

    Delete employee_management directory

--- employee_management/src/main/java/com/boarding/app/handler/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.boarding.app.handler;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(Long id) {
-        super("Could not find employee with id: " + id);
-    }
-}
--- employee_management/src/main/java/com/boarding/app/handler/TimesheetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.boarding.app.handler;
-
-public class TimesheetNotFoundException extends RuntimeException {
-    public TimesheetNotFoundException(Long id){
-        super("Could not find timesheet for this employee: " + id);
-    }
-}
