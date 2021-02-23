1e3fe5de2b1a01fd337834b6b2f6ab5369859698
gabrielbutnariu <butnariu.gabriel24@gmail.com>
2021-02-23 19:33:48 +0200

    added login page

--- employee-app-ui/src/app/app.component.spec.ts
+++ employee-app-ui/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'employee-app-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-app-ui');
+    expect(app.pageTitle).toEqual('employee-app-ui');
   });
 
   it('should render title', () => {
--- employee-app-ui/src/app/app.component.ts
+++ employee-app-ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // the selector name should start with app name(em-employee management) and should end with a name thats represents this component(root)
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-app-ui';
+  pageTitle  = 'Employee Management';
 }
