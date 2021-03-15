c8dc16951cac4c67ea9b512c6e2aa0126209eb0f
gabrielbutnariu <butnariu.gabriel24@gmail.com>
2021-03-15 15:15:55 +0200

    refactor home component and added reports ui

--- employee-app-ui/src/app/app.module.ts
+++ employee-app-ui/src/app/app.module.ts
@@ -25,6 +25,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatSelectModule} from '@angular/material/select';
 import {ModalComponent} from './components/modals/modal/modal.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
